69419a9ef57c206b3626574d6b7825d97ff64c0d
Daniel Bleisteiner <daniel.bleisteiner@ecg.de>
2019-02-20 16:49:08 +0100

    Standard Battle beim Start Ein paar kleine Anpassungen am Terminator

--- src/main/java/de/da3x/robocode/robots/Terminator.java
+++ src/main/java/de/da3x/robocode/robots/Terminator.java
@@ -14,30 +14,24 @@ public class Terminator extends AdvancedRobot {
 
     @Override
     public void run() {
+        // Wir wollen grundsätzlich immer in Bewegung bleiben...
         while (true) {
-
-            // Zuerst mal wollen wir stur geradeaus laufen...
             this.setAhead(500);
-            // Außerdem soll sich unsere Kanone permanent drehen und scannen...
             this.turnGunRight(360);
-
-            // Die soeben vorbereiteten Aktionen führen wir nun aus!
             this.execute();
-            while (this.getDistanceRemaining() > 0 && this.getTurnRemaining() > 0) {
-                this.execute();
-            }
         }
     }
 
     @Override
     public void onScannedRobot(ScannedRobotEvent event) {
         // Je näher der Gegner unserer Kanone ist, desto mehr Energie stecken wir in den Schuss!
+        // Allerdings schießen wir nur, wenn der Gegner nah genug ist. Alles andere wäre Verschwendung.
         this.fire(200 / event.getDistance());
     }
 
     @Override
     public void onHitWall(HitWallEvent event) {
-        // Wenn wir eine Wand treffen drehen wir uns einfach mal um 90 Grad und machen weiter.
-        this.setTurnLeft(90);
+        // Wenn wir eine Wand treffen drehen wir uns einfach mal um 90 Grad weg.
+        this.turnLeft(90);
     }
 }
